39d73d59fbc45dc33fcd96daf4ffe4d4a8f7d818
iMu90 <mtb.deco@gmail.com>
2018-08-02 01:25:10 +0300

    adding voice recognition

--- app/src/main/java/com/example/abdullah/mutmaen/MainActivity.kt
+++ app/src/main/java/com/example/abdullah/mutmaen/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.abdullah.mutmaen
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity(){
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        speak.setOnClickListener{
+            var intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+            startActivityForResult(intent, 10)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            10 -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    var res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    speakTxt.text = res.get(0)
+                }
+            }
+        }
+    }
+
 }
